9da21501519f96e955498333e4530500fd89ef0c
Bob D'Errico <bob@bobderrico.com>
2018-09-11 21:58:31 -0400

    Initial logic to get and group commits from github

--- source/app.js
+++ source/app.js
@@ -1,3 +1,42 @@
-const logger = console;
+const express = require('express');
+const request = require('request-promise-native');
 
-logger.info('hello world!');
+const port = 8080;
+const logger = console;
+const app = express();
+const githubApiUrl = 'https://api.github.com/repos/nodejs/node/commits';
+
+app.get('/', async (req, res) => {
+  try {
+    const groupedCommits = (await request(githubApiUrl, {
+      headers: { 'User-Agent': 'Request-Promise-Native' },
+      json: true,
+    }))
+      // Pull out just the values we need
+      .map(({ sha, commit }) => ({
+        sha,
+        authorName: commit.author.name,
+        authorEmail: commit.author.email,
+      }))
+
+      // Group by author email
+      .reduce((previousGroupedCommits, commit) => {
+        const key = commit.authorEmail;
+
+        if (!previousGroupedCommits[key]) {
+          return { ...previousGroupedCommits, [key]: [commit] };
+        }
+
+        const previousGroup = previousGroupedCommits[key];
+
+        return { ...previousGroupedCommits, [key]: [...previousGroup, commit] };
+      }, {});
+
+    res.status(200).json(groupedCommits);
+  } catch (error) {
+    logger.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.listen(port, () => logger.info(`Listening on port ${port}`));
